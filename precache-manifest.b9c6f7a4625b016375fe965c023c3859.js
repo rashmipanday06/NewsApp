6a8dab504bb123504fc69b83d2a26ed2aaa7dcf5
Rashmi <rashmipanday06598@gmail.com>
2022-02-28 22:50:25 +0530

    Initial commit

---
+++ precache-manifest.b9c6f7a4625b016375fe965c023c3859.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65034dfc1af499f7ae4bb86a9b0dd1af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebb8c2ca4511822f5d47",
+    "url": "./static/css/main.345509c7.chunk.css"
+  },
+  {
+    "revision": "890d0a702fd53d72969b",
+    "url": "./static/js/2.dba56f52.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "./static/js/2.dba56f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336daf59c3f28f2d684c",
+    "url": "./static/js/3.e83af0c0.chunk.js"
+  },
+  {
+    "revision": "ebb8c2ca4511822f5d47",
+    "url": "./static/js/main.9c165ff1.chunk.js"
+  },
+  {
+    "revision": "02e8dad698e6dc31ae1f",
+    "url": "./static/js/runtime-main.5d52d090.js"
+  }
+]);
